58fae6072d3bfec1833bc245f7452fef9c6d70c5
yamadamuller <mateusyamada@alunos.utfpr.edu.br>
2025-08-10 11:22:35 -0300

    [framework] Created file_mat and the LaipseData structure to store the current from the experiments of the LAIPSE database

--- test/test_file_mat.py
+++ test/test_file_mat.py
@@ -1,5 +1,5 @@
 from framework import file_mat
 
-directory = '../data/labtest_1_broken_bar/' #directory with data is located in the directory prior
-data = file_mat.read(directory, 60) #organize the output in a SimuData structure
+directory = '../data/narco/struct_r1b_R1.mat' #directory with data is located in the directory prior
+data = file_mat.read(directory, 4, n_periods=600, exp_num=5) #organize the output in a LaipseData structure
 print(type(data))
